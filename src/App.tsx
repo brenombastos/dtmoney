87f7b29464dacb8fc72e9690d2b72b2c904c4276
brenombastos <brenodead@gmail.com>
2021-03-20 19:20:13 -0300

    Alterando estilos do Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { GlobalStyle } from "./assets/styles/global";
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import Modal from 'react-modal';
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
 
